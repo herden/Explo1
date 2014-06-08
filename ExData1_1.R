663b0d4f628bd8db2eda91beb24d95f431840d7c
Denis Saparov <herden@Deniss-iMac.local>
2014-06-08 14:57:54 -0700

    first commit

---
+++ ExData1_1.R
@@ -0,0 +1,16 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "./data/ExData/exdata%2Fdata%2Fhousehold_power_consumption.zip", method ="curl")
+data<-read.csv("./data/ExData/household_power_consumption.txt", sep=";")
+data$Date<-as.Date(data$Date, "%d/%m/%Y")
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
+data$Voltage<-as.numeric(data$Voltage)
+data$Global_intensity <-as.numeric(data$Global_intensity)
+data$Sub_metering_1 <-as.numeric(data$Sub_metering_1 )
+data$Sub_metering_2 <-as.numeric(data$Sub_metering_2 )
+data$Sub_metering_3 <-as.numeric(data$Sub_metering_3 )
+data2days <- data[data$Date >= as.Date("2007-02-01") & data$Date<=as.Date("2007-02-02"), ]
+
+png(filename="plot1.png", width=480, height=480)
+hist(data2days$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+dev.off()
+hist(data2days$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
